0dc343f68e730684b8ed88f7e835712419a84ccc
Marc Morant <marc-mrt@users.noreply.github.com>
2022-06-11 16:34:42 +0200

    chore(aws_miniflux): add instance id output

--- modules/aws_miniflux/outputs.tf
+++ modules/aws_miniflux/outputs.tf
@@ -1,3 +1,8 @@
+output "instance_id" {
+  value       = aws_instance.miniflux.id
+  description = "The instance ID"
+}
+
 output "instance_public_ip" {
   value       = aws_instance.miniflux.public_ip
   description = "The instance's public IP address"
